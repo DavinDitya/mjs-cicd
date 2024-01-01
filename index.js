40f1d00f0cb4657aae8c5a7d290934c3bfaca543
DavinDitya <114983539+DavinDitya@users.noreply.github.com>
2024-01-01 10:15:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('( "response": "Halo!" )');
+  res.send('( "response": "Halo!!" )');
 });
 
 app.get("/will", function (req, res) {
-  res.send('( "response": "Selamat Datang!" )');
+  res.send('( "response": "Selamat Datang!!" )');
 });
 app.get("/ready", function (req, res) {
   res.send('{ "response": "YESSSSSSSS BERHASILLLLLLL" }');
